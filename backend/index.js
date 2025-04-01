16e19944a364a243809581629afdfd6421b454cb
yujian <645113027@qq.com>
2025-04-01 02:14:32 -0400

    Rebuild with updated env

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // ✅ handle preflight requests
+app.options('*', cors(corsOptions)); 
 
 app.use(express.json());
 
